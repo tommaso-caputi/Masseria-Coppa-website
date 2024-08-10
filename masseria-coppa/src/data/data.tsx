a15d8cc639fe0c0d7a0107460f781203d8bc51b3
tommaso caputi <56504768+tommaso-caputi@users.noreply.github.com>
2024-08-10 12:03:12 +0200

    quick fix

--- masseria-coppa/src/data/data.tsx
+++ masseria-coppa/src/data/data.tsx
@@ -1,5 +1,5 @@
 export const latticini = [
-    { nome: "Scamorze", descrizione: "" }
+    { nome: "Scamorze", descrizione: "" }, { nome: "Mozzarelle", descrizione: "" }, { nome: "Nodini", descrizione: "" }, { nome: "Ricotta", descrizione: "" }
 ]
 
 export const confetture = [
