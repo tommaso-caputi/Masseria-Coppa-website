c0a26a54da350ed9100d11a0805905d84e59237b
tommaso caputi <56504768+tommaso-caputi@users.noreply.github.com>
2024-08-10 11:51:01 +0200

    set footer base

--- masseria-coppa/src/components/Footer.tsx
+++ masseria-coppa/src/components/Footer.tsx
@@ -4,13 +4,10 @@ export default function Footer() {
     return (
         <>
             <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-                <p className="text-xs text-muted-foreground">&copy; 2024 Masseria Coppa. All rights reserved.</p>
+                <p className="text-xs text-muted-foreground">&copy; 2024 Ragione sociale | P.iva partita iva | Numero R.E.A numero rea</p>
                 <nav className="sm:ml-auto flex gap-4 sm:gap-6">
                     <Link href="#" className="text-xs hover:underline underline-offset-4" prefetch={false}>
-                        Terms of Service
-                    </Link>
-                    <Link href="#" className="text-xs hover:underline underline-offset-4" prefetch={false}>
-                        Privacy
+                        Dati identificativi
                     </Link>
                 </nav>
             </footer>
